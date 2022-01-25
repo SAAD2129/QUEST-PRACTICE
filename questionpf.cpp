b9374f9f0812af0fb5899a26b75aa5a48b3f78b0
CODExSAAD <mrsaad2129@gmail.com>
2022-01-25 15:59:56 +0500

    added new file

--- questionpf.cpp
+++ questionpf.cpp
@@ -11,13 +11,13 @@ int main()
     {
     sum=sum+sqrt(i+1/i);
     sum1=sum1+sqrt(i/i+1);
-    sum2=sum2+pow(i/i+1,2);
-    sum3=sum3+pow(i+1/i,2);
+    // sum2=sum2+pow(i/i+1,2);
+    // sum3=sum3+pow(i+1/i,2);
     
     }
     cout << sum<<endl;
     cout << sum1<<endl;
-    cout << sum2<<endl;
-    cout << sum3;
+    // cout << sum2<<endl;
+    // cout << sum3;
     return 0;
 }
