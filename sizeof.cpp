9e7177e22436f161ac5613c927dd424257143535
CODExSAAD <mrsaad2129@gmail.com>
2022-01-25 15:58:58 +0500

    sizeof program

---
+++ sizeof.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "The size of int is " << sizeof(int);
+    cout << "\nThe size of float is " << sizeof(float);
+    cout << "\nThe size of double is " << sizeof(double);
+    cout << "\nThe size of char is " << sizeof(char);
+    cout << "\nThe size of long double is " << sizeof(long double);
+    cout << "\nThe size of bool is " << sizeof(bool);
+    cout << "\nThe size of unsigned int  is " << sizeof(unsigned int);
+
+    return 0;
+}
