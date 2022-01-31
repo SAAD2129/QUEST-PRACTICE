cb28ebbc54ce26d0f700e4db6aaac2af6296cdf8
CODExSAAD <mrsaad2129@gmail.com>
2022-01-31 17:29:10 +0500

    added 31 january practice

---
+++ loops31.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    // divident
+    //  int divident, diviser;
+    //  char ch;
+    //  do
+    //  {
+    //      cout << "Enter the divident ";
+    //      cin >> divident;
+    //      cout << "\nEnter the diviser ";
+    //      cin >> diviser;
+    //      cout << "\nThe Quotient is " << divident / diviser;
+
+    //     cout << "\nThe Remainder is " << divident % diviser;
+
+    //     cout << "\nEnter y to continue & n to exit the program ";
+    //     cin >> ch;
+    //     /* code */
+    // } while (ch != 'n');
+
+    // for triangular pattern
+    // int rows;
+    // cout << "Enter the number of rows ";
+    // cin >> rows;
+    // for (int i = 1; i <= rows; i++)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         cout << "* ";
+    //     }
+    //     cout << "\n";
+    // }
+
+    // for reverse triangular &traingular pattern
+    //  int rows;
+    //  cout << "Enter the number of rows ";
+    //  cin >> rows;
+    // cout<<"Triangular Pattern & Reverse Triangular Pattern";
+    //  for (int i = 1; i <= rows+1; i++)
+    //  {
+    //      for (int j = 1; j <= i; j++)
+    //      {
+    //          cout << "* ";
+    //      }
+    //      cout << "\n";
+    //  }
+
+    // for (int i = rows; i >= 1; i--)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         cout << "* ";
+    //     }
+    //     cout << "\n";
+    // }
+
+    // while loop
+    //  int x, a, b;
+    //  x = 10;
+    //  a = x + 1;
+    //  b = a + 1;
+    //  do
+    //  {
+    //      cout << "\nThe value of x is " << x;
+    //      cout << "\nThe value of a is " << a;
+    //      cout << "\nThe value of b is " << b;
+    //      /* code */
+    //  } while (x < 10);
+
+    // while loop
+    //  int x, a, b;
+    //  x = 10;
+    //  a = x + 1;
+    //  b = a + 1;
+    //  do
+    //  {
+    //      cout << "\nThe value of x is " << x;
+    //      cout << "\nThe value of a is " << a;
+    //      cout << "\nThe value of b is " << b;
+    //      /* code */
+    //      x--;
+    //      cout << "\n";
+    //  } while (x >= 1);
+
+    // for 1-5 i value
+    //  for (int i = 1; i <=5; i++)
+    //  {
+    //      for (int j = 1; j <=i; j++)
+    //      {
+    //             cout<<i;
+    //      }
+    //      cout<<"\n";
+    //  }
+
+    // for numbers 1-15
+    // int num = 0;
+    // for (int i = 1; i <= 5; i++)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+
+    //         num++;
+    //         cout << num << " ";
+    //     }
+
+    //     cout << "\n";
+    // }
+
+    return 0;
+}
